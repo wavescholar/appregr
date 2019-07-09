f64a1c75453982fafe46e891cf44b09dcb331ffb
Bruce Campbell <bruce.bishop.campbell@gmail.com>
2019-07-08 22:14:49 -0400

    dev

--- R/modelfactory.R
+++ R/modelfactory.R
@@ -1,11 +1,11 @@
 #' Returns common models
-#' @param modelname character or 
+#' @param modelname character or
 #' @return list(lm.fit,df) an object of type lm and the training data
 #' @export
 getmodel <- function(modelname) {
 
     ## error handling
-    if (!( is.character(x))) {
+    if (!( is.character(modelname))) {
         stop('Wrong object type, only character is supported.')
     }
 
@@ -13,7 +13,7 @@ getmodel <- function(modelname) {
     {
         library(faraway)
         data(prostate, package="faraway")
-        df <-prostate 
+        df <-prostate
         lm.fit <- lm(lpsa ~ ., data=df)
         results <- list(fit=lm.fit,data=df)
         return(results)
@@ -23,12 +23,12 @@ getmodel <- function(modelname) {
 listavailable <-function(){
     available <- list()
     dictelem <- c(key='prostate',val="The prostate data frame has 97 rows and 9 columns. A study on 97 men with prostate cancer who were due to receive a radical prostatectomy.")
-    
+
     available = c(available,dictelem)
 
     dictelem <- c(key='count-data',val="Synthetic Count Data.")
-    
+
     available = c(available,dictelem)
-    
+
     print(available)
 }
