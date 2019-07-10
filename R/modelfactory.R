d148006adf88b2d25a0a6644c8ced95a204a05b7
Bruce Campbell <bruce@aloidia.solutions>
2019-07-10 14:20:51 -0400

    dev

--- R/modelfactory.R
+++ R/modelfactory.R
@@ -1,5 +1,5 @@
 #' Returns common models
-#' @param modelname character or 
+#' @param modelname character 
 #' @return list(lm.fit,df) an object of type lm and the training data
 #' @export
 getmodel <- function(modelname) {
@@ -11,10 +11,19 @@ getmodel <- function(modelname) {
 
     if (modelname=='prostate')
     {
-        library(faraway)
-        data(prostate, package="faraway")
-        df <-prostate 
+        #library(faraway)
+        #data(prostate, package="faraway")
+        df <-faraway::prostate
         lm.fit <- lm(lpsa ~ ., data=df)
+        results <- list(fit=lm.fit,data=df)
+        return(results)
+    }
+    if (modelname=='gala')
+    {
+        #library(faraway)
+        #data(gala, package="faraway")
+        df <-faraway::gala
+        lm.fit <- lm(Species ~ Area + Elevation + Nearest + Scruz  + Adjacent, data=gala, data=df)
         results <- list(fit=lm.fit,data=df)
         return(results)
     }
@@ -22,13 +31,16 @@ getmodel <- function(modelname) {
 
 listavailable <-function(){
     available <- list()
-    dictelem <- c(key='prostate',val="The prostate data frame has 97 rows and 9 columns. A study on 97 men with prostate cancer who were due to receive a radical prostatectomy.")
-    
+    dictelem <- c(key='prostate',val="The prostate data frame has 97 rows and 9 columns.
+                                A study on 97 men with prostate cancer who were due to receive a radical prostatectomy.")
+
     available = c(available,dictelem)
 
-    dictelem <- c(key='count-data',val="Synthetic Count Data.")
-    
+    dictelem <- c(key='gala',val="Species diversity on the Galapagos Islands There are 30 Galapagos islands and 7 variables in the dataset.
+                                  The relationship between the number of plant species and several geographic variables is of interest.
+                                  The original dataset contained several missing values which have been filled for convenience.")
+
     available = c(available,dictelem)
-    
+
     print(available)
 }
