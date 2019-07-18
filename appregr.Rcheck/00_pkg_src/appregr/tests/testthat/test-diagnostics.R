b5869f85e66484b5c41bacdff4134f2c447042b3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-18 00:21:03 +0000

    Deploy brucebcampbell/appregr to github.com/brucebcampbell/appregr.git:gh-pages

--- appregr.Rcheck/00_pkg_src/appregr/tests/testthat/test-diagnostics.R
+++
@@ -1,9 +0,0 @@
-test_that('Check checkoutliers', {  expect_is(
-  {
-    modelname<-'prostate'
-    mdf <- appregr::getmodel(modelname)
-    df <- mdf$data
-    lm.fit <- mdf$fit
-    appregr::checkleverage(lm.fit,df)
-  }
-  ,'data.frame')     })
